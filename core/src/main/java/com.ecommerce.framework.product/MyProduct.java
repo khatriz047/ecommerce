0dce7fb5a5f358bea8eea9ef2110a41f49cce7ad
Chandra Prakash <mun.lyt.5@gmail.com>
2016-05-17 22:28:08 -0500

    added my modules

---
+++ core/src/main/java/com.ecommerce.framework.product/MyProduct.java
@@ -0,0 +1,24 @@
+package com.ecommerce.framework.product;
+
+/**
+ * @author Chandra Prakash Panday
+ */
+public class MyProduct extends Product {
+	private double taxRate;
+
+	public MyProduct(String productId, String name, double price, int quantity, boolean activeFlag, double taxRate) {
+		super(productId, name, price, quantity, activeFlag);
+		this.taxRate = taxRate;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "MyProduct [taxRate=" + taxRate + "]";
+	}
+
+	
+	@Override
+	public void print(){
+		System.out.println(this.toString());
+	}
+}
---
+++ core/src/main/java/com.ecommerce.framework.product/Product.java
@@ -0,0 +1,58 @@
+package com.ecommerce.framework.product;
+
+/**
+ * @author Chandra Prakash Panday
+ */
+public class Product extends CatalogComponent {
+	private String productId;
+	private String name;
+	private double price;
+	private int quantity;
+	private boolean activeFlag;
+
+	public Product(String productId, String name, double price, int quantity, boolean activeFlag) {
+		this.productId = productId;
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		this.activeFlag = activeFlag;
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public double getPrice() {
+		return this.price;
+	}
+
+	@Override
+	public String getProductId() {
+		return productId;
+	}
+
+	@Override
+	public int getQuantity() {
+		return quantity;
+	}
+
+	@Override
+	public boolean isActiveFlag() {
+		return activeFlag;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("Product [productId=" + productId + ", name=" + name + ", price=" + price + ", quantity="
+				+ quantity + ", activeFlag=" + activeFlag + "]");
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", name=" + name + ", price=" + price + ", quantity=" + quantity
+				+ ", activeFlag=" + activeFlag + "]";
+	}
+
+}
