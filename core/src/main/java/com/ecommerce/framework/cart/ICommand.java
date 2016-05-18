5cbb2a376c23a2ae34306259af842b07f1a9495c
Chandra Prakash <mun.lyt.5@gmail.com>
2016-05-17 22:20:49 -0500

    Class name changed

--- core/src/main/java/com/ecommerce/framework/cart/ICommand.java
+++ core/src/main/java/com/ecommerce/framework/cart/ICommand.java
@@ -3,7 +3,7 @@ package com.ecommerce.framework.cart;
 /**
  * Created by Sandip on 5/17/16.
  */
-public interface ICommad {
+public interface ICommand {
 
     boolean execute();
     boolean undo();
